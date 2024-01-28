e453137997a41b93c989802780e2fe676ec05cb6
Daniel Merkl <daniel.merkl.dm@gmail.com>
2024-01-28 17:57:05 +0100

    zod schema validation for rgc event

--- src/model/RgcEvent.ts
+++ src/model/RgcEvent.ts
@@ -1,7 +1,15 @@
-export interface RgcEvent {
-  name: string;
-  start: Date;
-  end: Date;
-  ballroom: 1 | 2 | 3;
-  approved: false;
-}
+import { z } from "zod";
+import { Timestamp } from "firebase/firestore";
+
+export const rgcEventSchema = z.object({
+  name: z.string(),
+  start: z.custom<Timestamp>().transform((start) => start.toDate()),
+  end: z.custom<Timestamp>().transform((start) => start.toDate()),
+  ballroom: z.number().gte(1).lte(3),
+  approved: z.boolean(),
+  series: z.string().nullable(),
+});
+
+export type RgcEvent = z.infer<typeof rgcEventSchema>;
+
+export type RgcEventFirestore = z.input<typeof rgcEventSchema>;
